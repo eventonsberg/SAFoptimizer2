301a3d5cd2bf8c50710d2c16698b75df32c9f087
eventonsberg <even-kristian.tonsberg@ffi.no>
2026-02-02 12:32:39 +0100

    Added varying air defense cost functionality and visualizations

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from single_solution import display_single_solution
 from varying_missile_budget import display_varying_missile_budget
 from varying_f_and_ad_budget import display_varying_f_and_ad_budget
 from missile_budget_vs_f_and_ad_budget import display_varying_missile_budget_vs_f_and_ad_budget
+from varying_air_defense_cost import display_varying_air_defense_cost
 
 st.set_page_config(
     page_title="SAF optimizer",
@@ -17,21 +18,25 @@ input_data = display_input_fields()
 model_inputs = format_model_inputs(input_data)
 
 st.subheader("Optimal løsning")
-tab1, tab2, tab3, tab4 = st.tabs([
+tabs = st.tabs([
     "Enkeltløsning",
     "Varierende missilbudsjett",
     "Varierende fabrikk- og luftvernbudsjett",
-    "Missilbudsjett vs. fabrikk- og luftvernbudsjett"
+    "Missilbudsjett vs. fabrikk- og luftvernbudsjett",
+    "Varierende luftvernkostnad"
 ])
 
-with tab1:
+with tabs[0]:
     display_single_solution(*model_inputs)
 
-with tab2:
+with tabs[1]:
     display_varying_missile_budget(*model_inputs)
 
-with tab3:
+with tabs[2]:
     display_varying_f_and_ad_budget(*model_inputs)
 
-with tab4:
+with tabs[3]:
     display_varying_missile_budget_vs_f_and_ad_budget(*model_inputs)
+
+with tabs[4]:
+    display_varying_air_defense_cost(*model_inputs)
